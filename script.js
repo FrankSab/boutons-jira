81352bc0cc81a717e5ce8662e2c30b68377962f8
FrankSab <francois.sabatini@aucoeurdelavie.ca>
2025-08-07 14:47:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.getElementById("open-jira-create-issue").addEventListener("click", function() {
-  window.open("https://devfrancois.atlassian.net/secure/CreateIssue!default.jspa", "_blank");
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('open-create-issue');
+  if (button) {
+    button.addEventListener('click', function () {
+      window.location.href = "/secure/CreateIssue!default.jspa";
+    });
+  }
 });
+
 
 
