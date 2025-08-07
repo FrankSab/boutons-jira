72982333f6e340a2af112d72c3d5038d1c6042c2
FrankSab <francois.sabatini@aucoeurdelavie.ca>
2025-08-07 13:53:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('open-create-issue');
+  if (button) {
+    button.addEventListener('click', function () {
+      window.location.href = "/secure/CreateIssue!default.jspa";
+    });
+  }
+});
