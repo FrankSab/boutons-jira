284ccb25ae6f7e2ed3f0ad869c175d8e882e9fdd
FrankSab <francois.sabatini@aucoeurdelavie.ca>
2025-08-12 10:18:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
   if (!button) return;
 
   button.addEventListener('click', function () {
-    const url = 'https://devfrancois.atlassian.net/secure/CreateIssue!default.jspa?pid=10001&issuetype=10003';
-    top.location.href = url;
+    // Get issue key from the URL
+    const issueKey = window.location.pathname.split('/').pop();
+
+    // Call Jira API to get the "Nom" field
+    fetch(`/rest/api/3/issue/${issueKey}`)
+      .then(res => res.json())
+      .then(issue => {
+        const nomValue = issue.fields.customfield_10043;
+
+        // Encode and build the Create Issue URL
+        const url = `https://devfrancois.atlassian.net/secure/CreateIssue!default.jspa?pid=10001&issuetype=10003&customfield_10043=${encodeURIComponent(nomValue)}`;
+        top.location.href = url;
+      });
   });
 });
 
