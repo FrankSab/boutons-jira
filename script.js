2c85ffbbaee5b74452a1136a29785f0d0f47aef2
FrankSab <francois.sabatini@aucoeurdelavie.ca>
2025-08-12 10:23:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const button = document.getElementById('my-blue-btn');
-  if (!button) return;
+AP.context.getContext(function (context) {
+  const issueKey = context.jira.issue.key;
 
-  button.addEventListener('click', function () {
-    // Get issue key from the URL
-    const issueKey = window.location.pathname.split('/').pop();
-
-    // Call Jira API to get the "Nom" field
-    fetch(`/rest/api/3/issue/${issueKey}`)
-      .then(res => res.json())
-      .then(issue => {
-        const nomValue = issue.fields.customfield_10043;
-
-        // Encode and build the Create Issue URL
-        const url = `https://devfrancois.atlassian.net/secure/CreateIssue!default.jspa?pid=10001&issuetype=10003&customfield_10043=${encodeURIComponent(nomValue)}`;
-        top.location.href = url;
-      });
+  fetch(`/rest/api/3/issue/${issueKey}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(issue => {
+    const nomValue = issue.fields.customfield_10043 || '';
+    const url = `https://devfrancois.atlassian.net/secure/CreateIssue!default.jspa?pid=10001&issuetype=10003&customfield_10043=${encodeURIComponent(nomValue)}`;
+    top.location.href = url;
   });
 });
 
