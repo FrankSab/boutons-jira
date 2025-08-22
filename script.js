3f439aca0182a656fe341568535ca375ecf6c521
FrankSab <francois.sabatini@aucoeurdelavie.ca>
2025-08-22 15:28:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Fallback URL in case REST fails
     let fallbackUrl = `${jiraBase}/secure/CreateIssueDetails!init.jspa?pid=10001&issuetype=10003`;
     fallbackUrl += '&summary=' + encodeURIComponent(issueKey);
-    fallbackUrl += '&description=' + encodeURIComponent("Trigger =" + issueKey);
+    fallbackUrl += '&description=' + encodeURIComponent(issueKey);
 
     // If AdaptavistBridge.request is available, fetch issue fields
     if (window.AdaptavistBridge?.request) {
